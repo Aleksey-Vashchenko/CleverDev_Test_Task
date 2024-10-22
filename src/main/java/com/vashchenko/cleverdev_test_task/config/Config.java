7eea14315b9bf4e327cffcd2ee453ae379e552bc
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-10-22 05:10:20 +0300

    import system: add task executor

---
+++ src/main/java/com/vashchenko/cleverdev_test_task/config/Config.java
@@ -0,0 +1,22 @@
+package com.vashchenko.cleverdev_test_task.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class Config {
+
+    @Bean(name = "TaskExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(7);
+        executor.setMaxPoolSize(42);
+        executor.setQueueCapacity(11);
+        executor.setThreadNamePrefix("TaskExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
